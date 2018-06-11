97cb069e5f894d2c65a645bb88387bfe906dbdaf
ZIMONH <zh@zimonh.at>
2018-06-11 12:19:58 +0200

    pretty stable

--- index.php
+++ index.php
@@ -1,8 +1,119 @@
-<?php
-	$d = '';
-	$d = '?'.date("Y.m.d.h.m.s");
-
-	include 'first.php';
-	include 'curency.html';
-	include 'end.php';
+<?php $d = '';
+//$d = '?'.date("Y.m.d.h.m.s");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>Currency Converter</title>
+
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" 	content="IE=edge">
+	<meta property="og:title" 			content="Currency Converter by ZIMONH" />
+	<meta property="og:type" 			content="website"  />
+	<meta property="og:url" 			content="http://convert.zimonh.at" />
+	<meta property="og:image" 			content="OGcoverCur.jpg" />
+	<meta property="og:image:secure_url" content="OGcoverCur.jpg" />
+	<meta property="og:image:width"		content="1200" />
+	<meta property="og:image:height" 	content="630" />
+	<meta property="og:image:type" 		content="image/jpeg" />
+	<meta property="og:description" 	content="Get free currency rates while you type using the latest or custom dates. Calculate the exchange rate. A powerful calculator to compare valuta." />
+	<meta name='description' 			content='Get free currency rates while you type using the latest or custom dates. Calculate the exchange rate. A powerful calculator to compare valuta.'>
+	<meta name="viewport" 				content="width = device-width, initial-scale = 1">
+
+
+	<link rel="icon" type="image/png" href="https://zimonh.at/zh/img/zh.ico">
+
+	<link   integrity="sha256-916EbMg70RQy9LHiGkXzG8hSg9EdNy97GazNG/aiY1w=" crossorigin="anonymous" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"/>
+	<script integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script integrity="sha256-KM512VNnjElC30ehFwehXjx1YCHPiQkOPmqnrWtpccM=" crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+	<link   href="style.css?<?php echo $d; ?>" rel="stylesheet">
+</head>
+<body>
+	<div class="overlay hidden-sm hidden-xs"></div>
+		<div class="container-fluid">
+			<div id="stylepicker" class="container text-center" style="margin-top:6vh; margin-bottom:70px;">
+
+
+				<div class="col-lg-12" style="text-align:center; font-family: Helvetica, sans-serif;">
+					<h1 style="color: white; border-radius: 7px; padding: 17px; text-shadow: 0px 0px 18px black;" ><b>Currency Converter</b><span style="font-weight: normal; " >. Made by </span><b><a href="http://zimonh.at">ZIMONH</a></b></h1>
+					<br>
+					<br>
+					<div class="input-group hideatpageload maxwidther">
+					  <input id="curencyfinder" type="text" class="form-control" placeholder="Search by Country or Currency.">
+					  <span class="input-group-btn">
+						<button id="scrollgo" class="btn btn-default btn-success" type="button">Go!</button>
+					  </span>
+					</div>
+				</div>
+
+
+				<div class="col-lg-6 col-xs-12">
+					<div class="form-group" style="margin-top:10px; margin-bottom:0px; ">
+						<div class='input-group date hideatpageload maxwidther' id='datetimepicker1'>
+							<input id="datetimepicker2" type='text' class="form-control" style="text-align:center;  font-size: 1.5em; height: 50px; padding: 10px 16px;" 	/>
+							<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-xs-12 hideatpageload">
+					<div class="input-group maxwidther" style="margin-top:10px; ">
+					  <span style="max-width:60px; width:100%; font-size: 1.5em; height: 50px; padding: 10px 16px; border-top-left-radius: 8px; border-bottom-left-radius: 8px;" class="input-group-addon" id="basic-addon1"><span class="hidden-xs">Places after decimal</span><span class="hidden-lg hidden-md hidden-sm">Decimal Places</span></span>
+					  <select style="text-align:center; width:100px; font-size: 1.5em; height: 50px; padding: 10px 16px;" class="form-control" id="selectdecim">
+							<option>0</option>
+							<option>1</option>
+							<option selected>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+							<option>6</option>
+						</select>
+					</div>
+				</div>
+
+
+				<div class="col-lg-12">
+					<div class="loaderrropacity loaderrr" style="pointer-events: none;" >
+						<div class="lds-css" style="  width: 184px; margin: 0 auto; height:130px; margin-top:-67px; " >
+							<div style="width:100%;height:100%;" class="lds-ellipsis">
+							<div><div></div></div>
+							<div><div></div></div>
+							<div><div></div></div>
+							<div><div></div></div>
+							<div><div></div></div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<form class="form-inline" style="display:inline-block;" >
+					<div class="centercurrency hideatpageload">
+
+					<p class="hidden-lg hidden-md hidden-sm" style="color:white;">Click currency to move it to top</p>
+					<div style="float:left;clear:both" id="psP" class="nubeT"></div>
+
+					<script src="script.js?<?php echo $d; ?>"></script>
+					<script integrity="sha256-LbneT1/CeDfUKV3znZTDTMwzbDHQIyL398rWmujjONo=" crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/mouse0270-bootstrap-notify/3.1.3/bootstrap-notify.min.js"></script>
+					<script integrity="sha256-Cjux44IGDGmZwm+qw4rtfj1swD9zdqmja4gaflupI8o=" crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+					<script integrity="sha256-5YmaxAwMjIpMrVlK84Y/+NjCpKnFYa8bWWBbUHSBGfU=" crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+					<link   integrity="sha256-yMjaV542P+q1RnH6XByCPDfUFhmOafWbeLPmqKh11zo=" crossorigin="anonymous" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css"/>
+				</div>
+			</form>
+			<br>
+			<br>
+			<br>
+		</div>
+	</div>
+	<footer class="footer" style="" >
+		<div class="container  text-center" style="padding-bottom: 10px; padding-top: 10px;" >
+			<span class="text-muted">&#9400; <a href="http://zimonh.at">ZIMONH</a> Exchange rates from <a href="http://fixer.io/">fixer.io</a></span>
+		</div>
+    </footer>
+
+	<script src="http://www.zimonh.at/userTrack/tracker.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,354 @@
+//jshint  esnext:true
+const countries	=
+	[{title: 'Euro',
+	search: 'Andorra Austria Belgium Cyprus Estonia Finland France Germany Greece Ireland Italy Kosovo Latvia Luxembourg Malta Monaco Montenegro Netherlands Portugal San Marino Slovakia Slovenia Spain Vatican City',
+	button: '€ EUR',
+	name: 'EUR'},
+
+	{title: 'United States Dollar',
+	search: 'Commonwealth of Puerto Rico Ecuador Republic of El Salvador Republic of Zimbabwe Guam US and British Virgin Islands Democratic Republic of Timor-Leste American Samoa Commonwealth of the Northern Mariana Islands Federated States of Micronesia Republic of Palau',
+	button: '$ USD',
+	name: 'USD'},
+
+	{title: 'China Yuan Renminbi',
+	search: 'People\'s Republic of China',
+	button: '￥ RMB',
+	name: 'CNY'},
+
+	{title: 'Australia Dollar',
+	search: 'Australia Kiribati Naura Papua New Guinea Tonga Tuvalu',
+	button: '$ AUD',
+	name: 'AUD'},
+
+	{title: 'Bulgarian Lev',
+	search: 'Bulgaria',
+	button: 'лв BGN',
+	name: 'BGN'},
+
+	{title: 'Brazilian Real',
+	search: 'Brazil',
+	button: 'R$ BRL',
+	name: 'BRL'},
+
+	{title: 'Canadian Dollar',
+	search: '$ CAD Canada',
+	button: '$ CAD',
+	name: 'CAD'},
+
+	{title: 'Swiss franc',
+	search: 'CHF Switzerland Liechtenstein Campione d\'Italia',
+	button: 'CHF',
+	name: 'CHF'},
+
+	{title: 'Czech koruna',
+	search: 'Czech Republic',
+	button: 'Kč CZK',
+	name: 'CZK'},
+
+	{title: 'Danish krone',
+	search: 'Denmark Greenland Faroe Islands',
+	button: 'kr DKK',
+	name: 'DKK'},
+
+	{title: 'Pound sterling',
+	search: 'UK United Kingdom Guernsey Isle of Man Jersey',
+	button: '£ GBP',
+	name: 'GBP'},
+
+	{title: 'Hong Kong dollar',
+	search: 'Hong Kong',
+	button: '$ HKD',
+	name: 'HKD'},
+
+	{title: 'Croatian kuna',
+	search: 'Croatia',
+	button: 'kn HRK',
+	name: 'HRK'},
+
+	{title: 'Hungarian forint',
+	search: 'Hungary',
+	button: 'Ft HUF',
+	name: 'HUF'},
+
+	{title: 'Indonesian rupiah',
+	search: 'Indonesia',
+	button: 'Rp IDR',
+	name: 'IDR'},
+
+	{title: 'Israeli new shekel',
+	search: 'Israel Palestinian Authority',
+	button: '₪ ILS',
+	name: 'ILS'},
+
+	{title: 'Indian rupee',
+	search: 'India',
+	button: 'INR',
+	name: 'INR'},
+
+	{title: 'Japanese yen',
+	search: 'Japan',
+	button: '¥ JPY',
+	name: 'JPY'},
+
+	{title: 'South Korean won',
+	search: 'South Korea',
+	button: '₩ KRW',
+	name: 'KRW'},
+
+	{title: 'Mexican peso',
+	search: 'Mexico',
+	button: '$ MXN',
+	name: 'MXN'},
+
+	{title: 'Malaysia ringgit',
+	search: 'Malaysia',
+	button: 'RM MYR',
+	name: 'MYR'},
+
+	{title: 'Norwegian krone',
+	search: 'Norway',
+	button: 'kr NOK',
+	name: 'NOK'},
+
+	{title: 'New Zealand Dollar',
+	search: 'New Zealand',
+	button: '$ NZD',
+	name: 'NZD'},
+
+	{title: 'Philippine peso',
+	search: 'Philippines',
+	button: '₱ PHP',
+	name: 'PHP'},
+
+	{title: 'Polish złoty',
+	search: 'Poland',
+	button: 'zł PLN',
+	name: 'PLN'},
+
+	{title: 'Romanian leu',
+	search: 'Romania',
+	button: 'lei RON',
+	name: 'RON'},
+
+	{title: 'Russian ruble',
+	search: 'Russia',
+	button: '₽ RUB',
+	name: 'RUB'},
+
+	{title: 'Swedish krona',
+	search: 'Sweden',
+	button: 'kr SEK',
+	name: 'SEK'},
+
+	{title: 'Singapore dollar',
+	search: 'Singapore Brunei',
+	button: '$ SGD',
+	name: 'SGD'},
+
+	{title: 'Thai baht',
+	search: 'Thailand',
+	button: '฿ THB',
+	name: 'THB'},
+
+	{title: 'Turkish lira',
+	search: 'Turkey Northern Cyprus',
+	button: '₺ TRY',
+	name: 'TRY'},
+
+	{title: 'South African rand',
+	search: 'South Africa Lesotho Namibia Swaziland',
+	button: 'R ZAR',
+	name: 'ZAR'}];
+
+let elements = '';
+
+countries.map((v)=>{
+	elements += `
+	<div class="sortable col-lg-4 col-md-6 col-xs-12">
+		<div class="input-group">
+			<span class="input-group-addon label"></span><span class="input-group-addon currencybutton" title="${v.title}" data-searchterm="${v.search}">${v.button}</span>
+			<div class="input-group-lg"><input type="number" min="0" name="${v.name}" class="form-control inputnronly" placeholder="" step="1"/></div>
+		</div>
+	</div>`;});
+
+$('#psP').html(elements);
+
+
+let behindthecomma = 2, date, rates, inputs;
+const fixerio = 'http://data.fixer.io/';
+const access_key = 'access_key=1f47ec7e6beadfec86dfbccdc0f731c7';
+
+function convert(countrycode = 'EUR'){
+	//this is the input being filled. countrycode
+	const value = inputs[countrycode].value;
+	//first convert to euros and then update all
+	const euro = value / rates[countrycode];
+	//take the euro value and multiply it by the exchange rate
+	for(let fields of inputs){
+
+		const name = fields.name;
+		if(name !== countrycode){
+		const rate = rates[name];
+		const product = euro * rate;
+		inputs[name].value = product.toFixed(behindthecomma);}	}
+}
+
+const exchange_rates = (recal, date = 'latest')=> {
+	const url = fixerio + date + '?base=EUR&'+access_key;
+	fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		rates = data.rates;
+		if(recal){
+			inputs.EUR.value = 1;
+			convert();
+		}
+	});
+};
+
+$(document).ready(function() {
+
+	inputs = $('.inputnronly');
+
+	for(let fields of inputs){
+		const name = fields.name;
+		inputs[name] = fields;}
+
+	//First load
+	exchange_rates(true);
+
+	//set default
+	$('#selectdecim').val(2);
+
+	$('.loaderrr').removeClass("loaderrropacity");
+	$('.inputnronly').css("pointerEvents", "auto");
+	$('.inputnronly').css("cursor", "auto");
+	$('.hideatpageload').removeClass("hideatpageload");
+	$(function() { $('#psP').sortable({ helper: 'clone' });});
+
+
+	$("#scrollgo").click(function(){$('html, body').animate({scrollTop: $('#searchgo').offset().top-30},1000);});
+
+
+	$('#curencyfinder').keyup(function() {
+
+		$('span.currencybutton').removeClass( "myClass" );
+		$('span.currencybutton').removeAttr('id', 'searchgo');
+
+		const find = $('#curencyfinder').val();
+
+		if(find !== ""){
+			const re =  RegExp(find ,"i");
+
+			$('span[title]').filter(function(){
+			   return re.test(this.title);
+			}).each(function(){
+				$(this).addClass( "myClass" );
+				$(this).attr('id', 'searchgo');
+			});
+
+			$('span[data-searchterm]').filter(function(){
+			   return re.test(this.getAttribute('data-searchterm'));
+			}).each(function(){
+				$(this).addClass( "myClass" );
+				$(this).attr('id', 'searchgo');
+			});
+		}
+	});
+
+
+	$(function () {
+	   var bindDatePicker = function() {
+			$(".date").datetimepicker({
+			format:'YYYY-MM-DD',
+				icons: {
+					time: "fa fa-clock-o",
+					date: "fa fa-calendar",
+					up: "fa fa-arrow-up",
+					down: "fa fa-arrow-down"
+				}
+			}).find('input:first').on("blur",function () {
+				// check if the date is correct. We can accept dd-mm-yyyy and yyyy-mm-dd.
+				// update the format if it's yyyy-mm-dd
+				var date = parseDate($(this).val());
+
+				//create date based on momentjs (we have that)
+				if(!isValidDate(date)) date = moment().format('YYYY-MM-DD');
+
+				$(this).val(date);
+			});
+		};
+
+	   const isValidDate = (value, format)=>{
+			format = format || false;
+			// lets parse the date to the best of our knowledge
+			if(format) value = parseDate(value);
+
+			var timestamp = Date.parse(value);
+
+			return isNaN(timestamp) === false;
+	   };
+
+		const parseDate = value=>{
+			var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
+			if (m) value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
+
+			return value;
+		};
+
+		bindDatePicker();
+	 });
+
+
+
+
+
+
+	/* Triggers */
+	$('.inputnronly').on('input', function() {
+		const cur = $(this).attr('name');
+		convert(cur);
+	});
+
+	/*$(".inputnronly").keydown(function(e) {
+		//46 is del    8 backspace    9 is tab       27 is esc     13 enter
+		if (
+			$.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190, 188]) !== -1 ||
+			(e.keyCode == 65 && e.ctrlKey === !0) ||
+			(e.keyCode == 67 && e.ctrlKey === !0) ||
+			(e.keyCode == 86 && e.ctrlKey === !0) ||
+			(e.keyCode >= 35 && e.keyCode <= 39)
+		) { return }
+		//48 is 1  57 is 9
+		if((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)){
+			e.preventDefault();}
+	});*/
+
+	$('.currencybutton').on('click', function() {
+		$(this).parent().parent().parent().prepend($(this).parent().parent());
+		$.notify({
+			message: $(this).html() + " Moved to the top."
+		},{
+			type: 'success',
+			placement: {
+				from: "bottom",
+				align: "center"
+			},
+			position: null,
+			offset: 60,
+		});
+		$(this).addClass("yourClass");
+	});
+	$('#selectdecim').change(function() {
+		console.log('comma value changed');
+		behindthecomma = $('#selectdecim').val();
+		convert();
+	});
+	$('#datetimepicker1').on('dp.change', function(e) {
+		console.log('Date changed');
+		date = $('#datetimepicker1').data('date');
+		exchange_rates(true,date);
+	});
+
+
+});
