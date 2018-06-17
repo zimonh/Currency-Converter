3c75211e9359bfd0e22d4802e5ce06853f0326c8
Simon <zimonh@hotmail.com>
2018-06-17 13:32:51 +0200

    Create currencies.js

---
+++ currencies.js
@@ -0,0 +1,160 @@
+const 	countries	=
+	[{title: 'Euro',
+	search: 'Andorra Austria Belgium Cyprus Estonia Finland France Germany Greece Ireland Italy Kosovo Latvia Luxembourg Malta Monaco Montenegro Netherlands Portugal San Marino Slovakia Slovenia Spain Vatican City',
+	button: '€ EUR',
+	name: 'EUR'},
+
+	{title: 'United States Dollar',
+	search: 'Commonwealth of Puerto Rico Ecuador Republic of El Salvador Republic of Zimbabwe Guam US and British Virgin Islands Democratic Republic of Timor-Leste American Samoa Commonwealth of the Northern Mariana Islands Federated States of Micronesia Republic of Palau',
+	button: '$ USD',
+	name: 'USD'},
+
+	{title: 'China Yuan Renminbi',
+	search: 'People\'s Republic of China',
+	button: '￥ RMB',
+	name: 'CNY'},
+
+	{title: 'Australia Dollar',
+	search: 'Australia Kiribati Naura Papua New Guinea Tonga Tuvalu',
+	button: '$ AUD',
+	name: 'AUD'},
+
+	{title: 'Bulgarian Lev',
+	search: 'Bulgaria',
+	button: 'лв BGN',
+	name: 'BGN'},
+
+	{title: 'Brazilian Real',
+	search: 'Brazil',
+	button: 'R$ BRL',
+	name: 'BRL'},
+
+	{title: 'Canadian Dollar',
+	search: '$ CAD Canada',
+	button: '$ CAD',
+	name: 'CAD'},
+
+	{title: 'Swiss franc',
+	search: 'CHF Switzerland Liechtenstein Campione d\'Italia',
+	button: 'CHF',
+	name: 'CHF'},
+
+	{title: 'Czech koruna',
+	search: 'Czech Republic',
+	button: 'Kč CZK',
+	name: 'CZK'},
+
+	{title: 'Danish krone',
+	search: 'Denmark Greenland Faroe Islands',
+	button: 'kr DKK',
+	name: 'DKK'},
+
+	{title: 'Pound sterling',
+	search: 'UK United Kingdom Guernsey Isle of Man Jersey',
+	button: '£ GBP',
+	name: 'GBP'},
+
+	{title: 'Hong Kong dollar',
+	search: 'Hong Kong',
+	button: '$ HKD',
+	name: 'HKD'},
+
+	{title: 'Croatian kuna',
+	search: 'Croatia',
+	button: 'kn HRK',
+	name: 'HRK'},
+
+	{title: 'Hungarian forint',
+	search: 'Hungary',
+	button: 'Ft HUF',
+	name: 'HUF'},
+
+	{title: 'Indonesian rupiah',
+	search: 'Indonesia',
+	button: 'Rp IDR',
+	name: 'IDR'},
+
+	{title: 'Israeli new shekel',
+	search: 'Israel Palestinian Authority',
+	button: '₪ ILS',
+	name: 'ILS'},
+
+	{title: 'Indian rupee',
+	search: 'India',
+	button: 'INR',
+	name: 'INR'},
+
+	{title: 'Japanese yen',
+	search: 'Japan',
+	button: '¥ JPY',
+	name: 'JPY'},
+
+	{title: 'South Korean won',
+	search: 'South Korea',
+	button: '₩ KRW',
+	name: 'KRW'},
+
+	{title: 'Mexican peso',
+	search: 'Mexico',
+	button: '$ MXN',
+	name: 'MXN'},
+
+	{title: 'Malaysia ringgit',
+	search: 'Malaysia',
+	button: 'RM MYR',
+	name: 'MYR'},
+
+	{title: 'Norwegian krone',
+	search: 'Norway',
+	button: 'kr NOK',
+	name: 'NOK'},
+
+	{title: 'New Zealand Dollar',
+	search: 'New Zealand',
+	button: '$ NZD',
+	name: 'NZD'},
+
+	{title: 'Philippine peso',
+	search: 'Philippines',
+	button: '₱ PHP',
+	name: 'PHP'},
+
+	{title: 'Polish złoty',
+	search: 'Poland',
+	button: 'zł PLN',
+	name: 'PLN'},
+
+	{title: 'Romanian leu',
+	search: 'Romania',
+	button: 'lei RON',
+	name: 'RON'},
+
+	{title: 'Russian ruble',
+	search: 'Russia',
+	button: '₽ RUB',
+	name: 'RUB'},
+
+	{title: 'Swedish krona',
+	search: 'Sweden',
+	button: 'kr SEK',
+	name: 'SEK'},
+
+	{title: 'Singapore dollar',
+	search: 'Singapore Brunei',
+	button: '$ SGD',
+	name: 'SGD'},
+
+	{title: 'Thai baht',
+	search: 'Thailand',
+	button: '฿ THB',
+	name: 'THB'},
+
+	{title: 'Turkish lira',
+	search: 'Turkey Northern Cyprus',
+	button: '₺ TRY',
+	name: 'TRY'},
+
+	{title: 'South African rand',
+	search: 'South Africa Lesotho Namibia Swaziland',
+	button: 'R ZAR',
+	name: 'ZAR'}];
